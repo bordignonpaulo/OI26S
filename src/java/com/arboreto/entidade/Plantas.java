d8c0ecd1a495c02020feb10bb3c605e8bbb2dea4
Maicon <Maicon@maicon>
2017-09-12 22:29:06 -0300

    Finalizacao das entidades

--- src/java/com/arboreto/entidade/Plantas.java
+++ src/java/com/arboreto/entidade/Plantas.java
@@ -2,8 +2,134 @@
 package com.arboreto.entidade;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class Plantas implements Serializable {
+@Id@GeneratedValue (strategy = GenerationType.IDENTITY)
+private long Id;
+private String nome;
+private String origem;
+private String latitude;
+private String longitude;
+private String nomeCientifico;
+private String caracteristicas;
+
+@ManyToOne
+private familiaArborea familia;
+
+@ManyToOne
+private Categorias categoria;
+
+    public Plantas() {
+        super();
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getNomeCientifico() {
+        return nomeCientifico;
+    }
+
+    public void setNomeCientifico(String nomeCientifico) {
+        this.nomeCientifico = nomeCientifico;
+    }
+
+    public String getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(String caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+
+    public familiaArborea getFamilia() {
+        return familia;
+    }
+
+    public void setFamilia(familiaArborea familia) {
+        this.familia = familia;
+    }
+
+    public Categorias getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categorias categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + (int) (this.Id ^ (this.Id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Plantas other = (Plantas) obj;
+        if (this.Id != other.Id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Plantas{" + "Id=" + Id + ", nome=" + nome + ", origem=" + origem + ", latitude=" + latitude + ", longitude=" + longitude + ", nomeCientifico=" + nomeCientifico + ", caracteristicas=" + caracteristicas + ", familia=" + familia + ", categoria=" + categoria + '}';
+    }
     
+    
+
+
     
 }
